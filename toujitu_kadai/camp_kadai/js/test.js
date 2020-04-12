7081b7f1903fe4464404c588e5fefe3de1bcacd7
rungo728 <rufii72865@gmail.com>
2020-04-12 18:43:11 +0900

    create test.js

---
+++ toujitu_kadai/camp_kadai/js/test.js
@@ -0,0 +1,31 @@
+console.log("はじめてのジャバスクリプト");
+console.log(23 + 5);
+console.log(2000 - 1800);
+console.log("18+5");
+
+var name = "こやま";
+var point = 90;
+
+console.log(name);
+
+//if分の練習
+if (point >= 80) {
+  console.log("素晴らしい！おめでとう");
+} else {
+  console.log("もう少し頑張りましょう");
+}
+
+var random = Math.floor(Math.random() * 5 + 1);
+console.log(random);
+
+if (random == 1) {
+  console.log("1が出ました");
+} else if (random == 2) {
+  console.log("2が出ました");
+} else if (random == 3) {
+  console.log("3が出ました");
+} else if (random == 4) {
+  console.log("4が出ました");
+} else if (random == 5) {
+  console.log("5が出ました");
+}
