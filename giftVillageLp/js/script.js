40080494db3d300b7e4f03ea6f1534022b307dcd
rungo728 <53888314+rungo728@users.noreply.github.com>
2020-10-24 16:12:31 +0900

    add smooth scroll and wow animate
    jsでアニメーションを追加

--- giftVillageLp/js/script.js
+++ giftVillageLp/js/script.js
@@ -1,5 +1,37 @@
-$(function($) {
-  // wowjs//フワッとさせるアニメーション
-  new WOW().init()
+$(function() {
   
+  // wowjsフワッとさせるアニメーション
+  new WOW().init()
+
+  // スクロール検知
+  $(window).on("scroll", function() {
+    // トップから100px以上スクロールしたら
+    if (100 < $(this).scrollTop()) {
+      // is-showクラスをつける
+      $('.footer-topLink').addClass( 'is-show' );
+    } else {
+      // 100pxを下回ったらis-showクラスを削除
+      $('.footer-topLink').removeClass( 'is-show' );
+    }
+  });
+  // smoothscroll
+  // #から始まるURLがクリックされた時
+  $('a[href^="#"]').click(function() {
+    // 移動速度を指定（ミリ秒）
+    let speed = 500;
+    // hrefで指定されたidを取得
+    let id = $(this).attr("href");
+    // idの値が#のみだったらターゲットをhtmlタグにしてトップへ戻るようにする
+    let target = $("#" == id ? "html" : id);
+    // ページのトップを基準にターゲットの位置を取得
+    let position = $(target).offset().top;
+    // ターゲットの位置までspeedの速度で移動
+    $("html, body").animate(
+      {
+        scrollTop: position 
+      },
+      speed , 'swing'
+    );
+    return false;
+  });
 })
