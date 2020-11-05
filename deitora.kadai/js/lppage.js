fe2c384f46c1469ba3a8f07c8cb210361dd833ad
rungo728 <53888314+rungo728@users.noreply.github.com>
2020-11-05 19:49:01 +0900

    add contact form
    お問い合わせフォームが使用できるようにgoogleformを使用し、実装。送信できる条件を決める。

--- deitora.kadai/js/lppage.js
+++ deitora.kadai/js/lppage.js
@@ -92,4 +92,54 @@ $(function () {
       }
     }
   });
+
+  // faq
+  $( '.js-faq' ).on( 'click', function() {
+    // 押すと降りてくるメニューを実装できます。アコーディオンメニュー
+    $( this ).find( '.js-faqA' ).stop().slideToggle( 300 );
+    // toggleClassはON,Offを切り替える動きを得意とする
+    $( this ).toggleClass( 'add-active' );
+  });
+
+  // contact
+  // googleform
+  let $form = $( '#js-form' )
+  $form.submit(function (e) { 
+    $.ajax({ 
+      url: $form.attr('action'), 
+      data: $form.serialize(), 
+      type: "POST", 
+      dataType: "xml", 
+      statusCode: { 
+        0: function () { 
+          //送信に成功したときの処理 
+          $form.slideUp()
+          $( '#js-success' ).slideDown()
+        }, 
+        200: function () { 
+          //送信に失敗したときの処理
+          $form.slideUp()
+          $( '#js-error' ).slideDown() 
+        } 
+      } 
+    }); 
+    return false; 
+  });
+  // form入力の確認
+  let $submit = $( '#js-submit' )
+  $( '#js-form input, #js-form textarea').on('change',function () {
+    if (
+      $('#js-form input[type="text"]').val() !== "" &&
+      $('#js-form input[type="email"]').val() !== "" &&
+      $('#js-form input[name="entry.563652281"] ').prop( 'checked') === true
+    ) {
+      // 全て入力された時
+      $submit.prop( 'disabled', false)
+      $submit.addClass( '-active')
+    }else{
+      // 入力されていない時
+      $submit.prop( 'disabled', true)
+      $submit.removeClass( '-active')
+    }    
+  })
 });
